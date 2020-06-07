ab05c04e605c464138c233eb7c310a1842ad094a
javromsev <56070548+javromsev@users.noreply.github.com>
2020-06-07 21:26:33 +0200

    Update capsule_test.py

--- capsule_test.py
+++ capsule_test.py
@@ -12,9 +12,9 @@ import numpy as np
 #准备训练数据
 batch_size = 128
 num_classes = 10
-img_rows, img_cols = 28, 28
+img_rows, img_cols = 32, 32
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
 x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
 x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
 
